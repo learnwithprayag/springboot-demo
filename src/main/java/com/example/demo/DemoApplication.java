e2e781a50c8a5417aa38c978b7969f4a2ca8f701
Prayag Sangode <prayag.rhce@gmail.com>
2025-10-12 16:20:51 +0000

    update pom.xml

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Spring Boot JAR Demo!";
+        return "Hello from Spring Boot Maven Demo!";
     }
 
     public static void main(String[] args) {
